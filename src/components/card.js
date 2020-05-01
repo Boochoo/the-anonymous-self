f32a2e048c9292bc2e17a840bd781c83fccf406a
Ermias Hailemicheal <ermias.hailemicheal@frantic.com>
2020-05-01 23:03:16 +0300

    removes unused classes

--- src/components/card.js
+++ src/components/card.js
@@ -1,14 +1,11 @@
 import React from "react"
 
 const Card = ({ node: { title, slug, image, description } }) => (
-  <div className="w-full md:w-1/2 lg:w-1/3 px-3 flex flex-col mb-8">
-    <a
-      href={slug}
-      className="no-underline bg-white rounded-lg shadow hover:shadow-raised hover:translateY-2px transition flex-1 flex flex-col overflow-hidden"
-    >
+  <div className="">
+    <a href={slug} className="">
       <div>
         <div
-          className="bg-cover aspect-ratio-16x9"
+          className=""
           style={{
             height: 200,
             backgroundImage: `url(https:${image.file.url}?w=640&h=360&fit=thumb)`,
@@ -16,12 +13,12 @@ const Card = ({ node: { title, slug, image, description } }) => (
         />
       </div>
       <div
-        className="p-6 flex-1 flex flex-col justify-between"
+        className=""
         style={{
           margin: "0.5rem 0.5rem 0 0.5rem",
         }}
       >
-        <h3 className="font-display text-black no-underline mb-4">{title}</h3>
+        <h3 className="">{title}</h3>
       </div>
     </a>
   </div>
